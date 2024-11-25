50dcfa9b50b400af8cbb53f1c210da1ebd4da60e
bearcattt <ewigglet8@gmail.com>
2024-11-25 11:36:29 -0500

    no attack doggy

--- assets/scripts/cloak.js
+++ assets/scripts/cloak.js
@@ -2,8 +2,8 @@ function cloak() {
 var win = window.open()
 var url = location.href;
 var iframe = win.document.createElement('iframe')
-iframe.style.width = "100%";
-iframe.style.height = "100%";
+iframe.style.width = "100vw";
+iframe.style.height = "100vh";
 iframe.style.border = "none";
 iframe.src = url
 win.document.body.appendChild(iframe)
