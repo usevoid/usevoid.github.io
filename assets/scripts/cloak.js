f2e2b0407aa39601390d8baa57c200841b86ee1d
bearcattt <ewigglet8@gmail.com>
2024-11-25 11:54:46 -0500

    Update cloak.js

--- assets/scripts/cloak.js
+++ assets/scripts/cloak.js
@@ -2,12 +2,12 @@ function cloak() {
 var win = window.open()
 var url = location.href;
 var iframe = win.document.createElement('iframe')
-iframe.style.width = "100";
-iframe.style.height = "100vh";
+iframe.style.width = "100%";
+iframe.style.height = "100%";
 iframe.style.border = "none";
 iframe.src = url
 win.document.body.appendChild(iframe)
-window.location.replace("https://google.com");
+
 }
 
 cloak()
