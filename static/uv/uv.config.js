416c22e2b443c5cc010bb2db1a24aa96dd9f3153
bearcattt <ewigglet8@gmail.com>
2024-12-18 14:45:37 -0500

    bare

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/nowgg/',
-    bare:'https://homes.renaca.com/fq/',
+    bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
